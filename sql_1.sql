06834e7e8a945ef9dc585c25701f68d62ab1952e
pokselet <96790850+pokselet@users.noreply.github.com>
2022-01-09 15:46:25 +0100

    Update sql_1.sql

--- sql_1.sql
+++ sql_1.sql
@@ -39,8 +39,10 @@ create table wykonawca
 drop table if exists utwor_instrument;
 create table utwor_instrument
 (
-    utwor_id      int references utwor(id),
-    instrument_id int references instrument(id),
+    utwor_id      int,
+    instrument_id int,
+    foreign key (utwor_id)references utwor(id),
+    foreign key (instrument_id)references instrument(id),
     primary key (utwor_id, instrument_id)
 );
 
